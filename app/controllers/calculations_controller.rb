009280765234e33d119be3d2492e8e472262d484
MarzB <mbrzakal@chicagobooth.edu>
2015-05-13 12:50:15 -0500

    RCAV Midterm Practice
    Completed part of RCAV practice to study for midterm

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,9 +2,25 @@ class CalculationsController < ApplicationController
   def instructions
   end
 
+  def square
+    @number = params[:number]
+    @square = @number.to_i * @number.to_i
+
+  end
+
   def sqrt
+    @number = params[:number]
+    @sqrt = Math.sqrt(@number.to_i)
+
   end
 
   def pmt
+    @int = params[:int_rate].to_f
+    @num_pmt = params[:pmt].to_f
+    @principal = params[:principal].to_f
+
+    rate = @int / 100
+
+    @loan_payment = (rate * @principal)/(1 - (1 + rate)**-@num_pmt)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "calculations", :action => "instructions" })
+  get("/",                                  { :controller => "calculations", :action => "instructions" })
+
+  get("/square/:number",                    { :controller => "calculations", :action => "square" })
+  get("/square_root/:number",               { :controller => "calculations", :action => "sqrt" })
+  get("/payment/:int_rate/:pmt/:principle", { :controller => "calculations", :action => "pmt" })
 end
